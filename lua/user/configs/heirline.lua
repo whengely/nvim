c182d0c289b210f1d0105e771a2cdd85a7d99e3f
Walt Hengely <whengely@igsenergy.com>
2022-09-15 17:44:27 -0400

    remove snippet notifier for heirline

--- lua/user/configs/heirline.lua
+++ lua/user/configs/heirline.lua
@@ -421,18 +421,6 @@ local Git = {
     },
 }
 
-local Snippets = {
-    condition = function()
-        return vim.tbl_contains({ "s", "i" }, vim.fn.mode())
-    end,
-    provider = function()
-        local forward = (vim.fn["UltiSnips#CanJumpForwards"]() == 1) and "" or ""
-        local backward = (vim.fn["UltiSnips#CanJumpBackwards"]() == 1) and " " or ""
-        return backward .. forward
-    end,
-    hl = { fg = "red", bold = true },
-}
-
 local DAPMessages = {
     condition = function()
         local session = require("dap").session()
@@ -556,7 +544,7 @@ local Spell = {
     hl = { bold = true, fg = "orange" },
 }
 
-ViMode = utils.surround({ "", "" }, "bright_bg", { ViMode, Snippets })
+ViMode = utils.surround({ "", "" }, "bright_bg", { ViMode })
 
 local Align = { provider = "%=" }
 local Space = { provider = " " }
